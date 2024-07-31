109a21df721429305ede12c958fd6544b090a63b
paolo flore <paoloflore937@gmail.com>
2024-07-31 18:34:31 +0200

    Aggiungere i file di progetto.

---
+++ libreria/Libro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libreria
+{
+    internal class Libro
+    {
+        public string titolo {  get; set; }
+        public Autore Autore { get; set; }
+        public string SBN { get; set; }
+        public DateTime Data {  get; set; }
+        public Libro()
+        {
+        
+        }
+    }
+}
---
+++ libreria/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace libreria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //utente agggiunge un libro titolo, autore, ISBN, anno
+            //cerca il libro 
+
+        }
+    }
+}
